f9c81361514f80c0b07e0c0ce5b2b9277ed95dda
Uzair Suria <uzair.a.suria@outlook.com>
2021-01-14 16:39:19 +0500

    added getToken method

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import './App.css';
 import ExpenseTracker from './containers/ExpenseTracker';
+import firebase from './firebase';
+import { register } from './serviceWorkerRegistration';
+// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 function App() {
-	Notification.requestPermission();
+	Notification.requestPermission()
+		.then(() => {
+			return firebase.messaging().getToken();
+		})
+		.then((token) => {
+			console.log(`Token:\n${token}`);
+		});
 	return (
 		<>
 			<div className="App">
